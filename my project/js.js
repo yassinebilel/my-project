fe04751cde75364f0ac08582a97ab38ae6e93940
nextwarrior000 <yassinesalem543@gmail.com>
2024-01-10 08:52:35 +0100

    my project

--- my project/js.js
+++ my project/js.js
@@ -6,3 +6,78 @@ $(document).ready(function () {
   });
 });
 
+// $(document).ready(function () {
+//   $(".pushable").click(function () {
+//     $(".myimagee").animate({
+//       height: "toggle",
+//     });
+//   });
+
+//   $(".next-btn").click(function () {
+//     $(".swiper").animate(
+//       {
+//         scrollLeft: "+=610",
+//       },
+//       "slow"
+//     );
+//   });
+
+//   $(".prev-btn").click(function () {
+//     $(".swiper").animate(
+//       {
+//         scrollLeft: "-=610",
+//       },
+//       "slow"
+//     );
+//   });
+// });
+
+// // var swiper = document.querySelector(".swiper");
+
+// // var myImage = document.querySelector(".myimagee");
+
+// // var prevBtn = document.querySelector(".prev-btn");
+// // var nextBtn = document.querySelector(".next-btn");
+// // console.log("prevBtn", prevBtn);
+// // console.log("nextBtn", nextBtn);
+// // nextBtn.addEventListener("click", () => {
+// //   swiper.scrollTo({
+// //     left: 610,
+// //     behavior: "smooth",
+// //   });
+// // });
+
+// // prevBtn.addEventListener("click", () => {
+// //   swiper.scrollTo({
+// //     left: -610,
+// //     behavior: "smooth",
+// //   });
+// // });
+
+$(document).ready(function () {
+  $(".next-btn").click(function () {
+    $(".swiper").animate(
+      {
+        scrollLeft: "+=610",
+      },
+      "slow"
+    );
+  });
+
+  $(".prev-btn").click(function () {
+    $(".swiper").animate(
+      {
+        scrollLeft: "-=610",
+      },
+      "slow"
+    );
+  });
+});
+
+$(document).ready(function () {
+  $("#trigger").click(function () {
+    $("#description").slideToggle();
+  });
+});
+
+
